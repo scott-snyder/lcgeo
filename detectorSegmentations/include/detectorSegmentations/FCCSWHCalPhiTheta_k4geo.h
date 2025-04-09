8efe43a8adb351a323f8d8adb6a54b3e3a242da5
sss <sss@karma>
2025-04-09 10:01:42 -0400

    hcal-seg-wip

--- detectorSegmentations/include/detectorSegmentations/FCCSWHCalPhiTheta_k4geo.h
+++ detectorSegmentations/include/detectorSegmentations/FCCSWHCalPhiTheta_k4geo.h
@@ -279,7 +279,6 @@ namespace DDSegmentation {
 
         // The z-center of the dd4hep volume.
         double volumeZ {0};
-      
       };
       std::vector<CellInfo> m_cellInfo1 {};
       std::vector<CellInfo> m_cellInfo2 {};
@@ -308,13 +307,14 @@ namespace DDSegmentation {
     const LayerInfo& getLayerInfo (const unsigned layer) const;
 
     /**  Construct the derived geometrical information.
+     *
      * Calculate layer radii and edges in z-axis, then define cell edges in each layer using defineCellEdges().
      *    Following member variables are calculated:
      *      radius
      *      layerEdges
      *      layerDepth
      *      thetaBins (updated through defineCellEdges())
-     *      cellEdges* (updated through defineCellEdges())
+     *      m_cellEdges* (updated through defineCellEdges())
      */
     std::vector<LayerInfo> initLayerInfo() const;
 
