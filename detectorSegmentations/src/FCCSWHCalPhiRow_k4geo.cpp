c50627a113876593712fb1fb532d58f051fac842
sss <sss@karma>
2025-08-29 23:06:40 -0400

    neighbor-dump-wip

--- detectorSegmentations/src/FCCSWHCalPhiRow_k4geo.cpp
+++ detectorSegmentations/src/FCCSWHCalPhiRow_k4geo.cpp
@@ -59,6 +59,7 @@ namespace DDSegmentation {
   Vector3D FCCSWHCalPhiRow_k4geo::position(const CellID& cID) const {
     uint layer = decoder()->get(cID, m_layerIndex);
 
+    dumpneighbors();
     const LayerInfo& li = getLayerInfo(layer);
 
     double radius = li.radius;
@@ -687,6 +688,44 @@ namespace DDSegmentation {
 
     return (M_PI - thetaMin); // theta max
   }
+
+std::vector<CellID> FCCSWHCalPhiRow_k4geo::allCells() const
+{
+  dd4hep::DDSegmentation::CellID cID = 0;
+  int id = m_detLayout == 0 ? 8 : 9;
+  decoder()->set(cID, "system", id);
+
+  std::vector<CellID> out;
+  int nl = std::ranges::fold_left (m_numLayers, 0, std::plus<int>());
+
+  for (int layer = 0; layer < nl; ++layer) {
+    const LayerInfo& li = getLayerInfo(layer);
+    decoder()->set(cID, m_layerIndex, layer);
+    for (int row : li.cellIndexes) {
+      decoder()->set(cID, m_rowIndex, row);
+      for (int phi = 0; phi < m_phiBins; ++phi) {
+        decoder()->set(cID, m_phiIndex, phi);
+        out.push_back(cID);
+      }
+    }
+  }
+  return out;
+}
+
+void FCCSWHCalPhiRow_k4geo::dumpneighbors() const
+{
+  if (m_dumped) return;
+  m_dumped = true;
+  FILE* fout = fopen ("out/neigh-row.dump", "w");
+  for (CellID cid : allCells()) {
+    fprintf (fout, "%lx ->", cid);
+    for (uint64_t n : this->neighbours(cid)) {
+      fprintf (fout, " %lx", n);
+    }
+    fprintf (fout, "\n");
+  }
+  fclose(fout);
+}
 
 } // namespace DDSegmentation
 } // namespace dd4hep
