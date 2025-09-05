a6ece0830d3a7f716edb5d1124a10b31ac042117
sss <sss@karma>
2025-09-05 13:45:19 -0400

    Refresh of segCleanup1

--- detectorSegmentations/include/detectorSegmentations/FCCSWGridPhiTheta_k4geo.h
+++ detectorSegmentations/include/detectorSegmentations/FCCSWGridPhiTheta_k4geo.h
@@ -76,7 +76,7 @@ namespace DDSegmentation {
      *  @param[in] cellID
      *  return a std::vector of size 2 with the cellDimensions of the given cell ID (phi, theta)
      */
-    inline std::vector<double> cellDimensions(const CellID /* id */) const { return {gridSizePhi(), gridSizeTheta()}; }
+    virtual std::vector<double> cellDimensions(const CellID& /* id */) const override { return {gridSizePhi(), gridSizeTheta()}; }
 
   private:
     /// determine the azimuthal angle phi based on the current cell ID
